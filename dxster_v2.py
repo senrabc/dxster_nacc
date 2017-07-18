17306a0307833eaba9980a12a8a76f849ba1d294
Christopher Barnes <senrabc@gmail.com>
2017-07-18 16:21:49 -0400

    add load functions for input file.

--- dxster_v2.py
+++ dxster_v2.py
@@ -1,68 +1,103 @@
-#!/usr/bin/env python                                                            
-"""                                                                                
-  _____        _____ _                                                             
- |  __ \      / ____| |                                                            
- | |  | |_  __ (___ | |_ ___ _ __                                                  
- | |  | \ \/ /\___ \| __/ _ \ '__|                                                 
- | |__| |>  < ____) | |_  __/ |                                                    
- |_____//_/\_\_____/ \__\___|_|V2                                                    
-                                                                                   
-Copyright 2017 Christopher P. Barnes <senrabc@gmail.com>                           
-Copyright 2017 Kevin Hanson <kshanson@ufl.edu>                                     
-                                                                                   
-Licensed under the Apache License, Version 2.0 (the "License");                    
-you may not use this file except in compliance with the License.                   
-You may obtain a copy of the License at                                            
-                                                                                   
-   http://www.apache.org/licenses/LICENSE-2.0                                      
-                                                                                   
-Unless required by applicable law or agreed to in writing, software                
-distributed under the License is distributed on an "AS IS" BASIS,                  
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.           
-See the License for the specific language governing permissions and                
-limitations under the License.                                                     
+#!/usr/bin/env python
+################################################################################
+#  _____        _____ _
+# |  __ \      / ____| |
+# | |  | |_  __ (___ | |_ ___ _ __
+# | |  | \ \/ /\___ \| __/ _ \ '__|
+# | |__| |>  < ____) | |_  __/ |
+# |_____//_/\_\_____/ \__\___|_|V2
+#
+# Copyright 2017 Christopher P. Barnes <senrabc@gmail.com>
+# Copyright 2017 Kevin Hanson <kshanson@ufl.edu>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+################################################################################
 
+"""Dxster
 
-DxSter V2.0.
-Usage:
-  dxster_v2.py [options] (<EFILE> <DFILE> <OFILE>)
+Usage: dxster_v2.py [options]
 
-Arguments:
-  EFILE  Pass in the ealgdx alogrithm lookup table csv file.                     
-  DFILE  Pass in the properly formatted csv file you want to process.            
-  OFILE  Pass in the name and path of the output csv file for the results.
 
 Options:
-  -h, --help    	Show this screen.
-  --version    		Show version.
-  -c, --cmd_option  	Example commandline option to pass in [default: 'no']
-
+    -h, --help
+    -c, --config=<file>  Path to the config file [TODO: MAKE THIS WORK]
+    -e, --ealgdx_data=<file> Full path to ealgdx.csv file [ex. '/home/senrabc/ealgdx.csv']
+    -i, --input_file=<file>  Path to CSV data file [ex. '/home/senrabc/mydata.csv']
+    -o, --output_file=<file>  Path to output CSV file [ex. '~/myoutput.csv']
+    -r, --property_here        THIS NEEDS REMOVAL: Start the server if yes [default: 'no']
 """
+
 from docopt import docopt
+import csv
+import sys
+import pprint
+
+#TODO: take inputs from a config file so you don't have to enumerate all args
+#       on command line
 
 
-# Enhanced Algorithmic Diagnosis class
-class Ealgdx(object):
-    def __init__(self, efile, dfile, ofile, **kwargs):
-        self.efile = efile
-        self.dfile = dfile
-        self.ofile = ofile
- 	# self.cmd_option = cmd_option
-    
+class Dxster(object):
+    #create dicts here to hold ref data files
+    dict_input_file=[]
+    dict_output_file={}
+    dict_ealgdx_file={}
+
+
+    def __init__(self, my_property, **kwargs):
+
+        # example object property from CLI arg: use or remove alter if uneeded
+        self.my_property = my_property
+
+        # optional properties
+        # using kwargs.pop will return '' if no arg present
+        self.input_file = kwargs.pop('input_file','')
+        self.output_file = kwargs.pop('output_file','')
+        self.ealgdx_file = kwargs.pop('ealgdx_data','')
+
+    def load_dict_input_file(self, input_file):
+        # This function should read the CSV file and load it into the class
+        # dict called 'dict_input_file' that will hold the data to be processed
+        reader = csv.DictReader(open(input_file, 'rb'))
+        for line in reader:
+            self.dict_input_file.append(line)
+        return  self.dict_input_file
+
+    def load_dict_ealgdx_file():
+        # This function should read the CSV file and load it into the class
+        # dict called 'dict_ealgdx_file' that will hold the reference algo data
+        print ''
+    #Sample Function for CLI args. REMOVE later
     def print_msg(self):
-        print self.efile
-        print self.dfile
-        print self.ofile
-        # print self.cmd_option
+        print self.my_property
+        print self.input_file
+        print self.output_file
+        print self.ealgdx_file
+
+        #test load of the input file
+        input_values = self.load_dict_input_file(self.input_file)
+        pprint.pprint(input_values)
 
 if __name__ == '__main__':
-    args = docopt(__doc__, version='DxSter v2')
+    args = docopt(__doc__)
+
+    #sample usage output for ali args form docopt. REMOVE later
     print args
-    run_dxster = Ealgdx(efile=args['<EFILE>'],
-			dfile=args['<DFILE>'],
-			ofile=args['<OFILE>'])
+    dxster = Dxster(
+        my_property=args['--property_here'],
+        input_file=args['--input_file'],
+        output_file=args['--output_file'],
+        ealgdx_data=args['--ealgdx_data']
 
-
-    run_dxster.print_msg()
-
-                                                                                 
+    )
+    dxster.print_msg()
