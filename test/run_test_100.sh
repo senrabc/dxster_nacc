857e7db2c12f7c36426f7e55d0e2fa592e907a26
Christopher Barnes <senrabc@gmail.com>
2018-06-05 13:50:14 -0400

    housekeeping for run scripts

--- test/run_test_100.sh
+++ test/run_test_100.sh
@@ -1,1 +1,9 @@
-python dxster_v2.py -i ../temp/top100_nacc_dataset_0302217.csv -e ../docs/ealgdx_algorithm_table.csv
+#pathing assumes you are running from the test folder
+
+
+python ../dxster_v2.py -i ../temp/top100_nacc_dataset_0302217.csv -e \
+../docs/ealgdx_algorithm_table.csv 
+
+
+# python ../dxster_v2.py -i ../temp/top100_nacc_dataset_0302217.csv -e \
+# ../docs/ealgdx_algorithm_table.csv -o output_file.txt
